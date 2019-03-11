81f01c026aba8b4ac6e9190409680ceac2c74cb0
Ahees <fdsparta@gmail.com>
2019-03-12 01:15:30 +0200

    1

--- work/app/js/common.js
+++ work/app/js/common.js
@@ -3,3 +3,28 @@ $(function() {
 	// Custom JS
 
 });
+
+new Vue({
+  el: '#app',
+  methods: {
+    say: function (message) {
+      //console.log(message)
+			axios({
+			    method: 'post',
+			    url: 'http://gen.test/pro.php',
+			    data: {
+			        firstName: 'Fred',
+			        lastName: 'Flintstone'
+			    }
+			})
+			.then(function(response) {
+
+			    console.log(response);
+
+			})
+			.catch(function (error) {
+			    console.log(error);
+			});
+    }
+  }
+})
