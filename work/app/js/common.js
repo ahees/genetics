7fdfa0bd7c1645b3c1205913de87046d2ad9fe04
Ahees <fdsparta@gmail.com>
2019-03-12 16:46:57 +0200

    1

--- work/app/js/common.js
+++ work/app/js/common.js
@@ -1,30 +1,47 @@
 $(function() {
 
 	// Custom JS
+	$('.block .answer .a').on('click', function(){
+		
+		// $('.block .answer .a').removeClass('active');
+		// $(this).parent().find('.a').removeClass('active');
+		// $(this).toggleClass('active');
+		$(this).css('background-color', 'tomato');
+	});
+
+
+	$('body').on('change', '.block .answer .a', function() { 
+		console.log('yes');
+	});
+	
+
 
 });
 
-new Vue({
-  el: '#app',
-  methods: {
-    say: function (message) {
-      //console.log(message)
-			axios({
-			    method: 'post',
-			    url: 'http://gen.test/pro.php',
-			    data: {
-			        firstName: 'Fred',
-			        lastName: 'Flintstone'
-			    }
-			})
-			.then(function(response) {
 
-			    console.log(response);
 
-			})
-			.catch(function (error) {
-			    console.log(error);
-			});
-    }
-  }
-})
+
+// new Vue({
+//   el: '#app',
+//   methods: {
+//     say: function (message) {
+//       //console.log(message)
+// 			axios({
+// 			    method: 'post',
+// 			    url: 'http://gen.test/pro.php',
+// 			    data: {
+// 			        firstName: 'Fred',
+// 			        lastName: 'Flintstone'
+// 			    }
+// 			})
+// 			.then(function(response) {
+
+// 			    console.log(response);
+
+// 			})
+// 			.catch(function (error) {
+// 			    console.log(error);
+// 			});
+//     }
+//   }
+// })
