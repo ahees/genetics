01c251356db4c418d6726362c89fb1424f4822a3
ahees <ahees@ex.ua>
2019-03-13 22:45:31 +0200

    1

--- work/app/js/common.js
+++ work/app/js/common.js
@@ -1,12 +1,25 @@
 $(function() {
 
+	$('.sub').on('click', function(){
+
+		var data = $('#form1').serializeArray();
+
+		// var q1 = $("input[name=q1]");
+		// var checkedValue = q1.filter(":checked").val();
+		
+		
+		// console.log(checkedValue)
+		console.log(data)
+
+	});
+
 	// Custom JS
 	$('.block .answer .a').on('click', function(){
 		
 		// $('.block .answer .a').removeClass('active');
 		// $(this).parent().find('.a').removeClass('active');
 		// $(this).toggleClass('active');
-		$(this).css('background-color', 'tomato');
+		
 	});
 
 
