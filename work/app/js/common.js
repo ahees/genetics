e0c383ee21f96a13187c76249205f1c02371dfb9
Ahees <fdsparta@gmail.com>
2019-03-14 16:41:30 +0200

    1

--- work/app/js/common.js
+++ work/app/js/common.js
@@ -1,47 +1,19 @@
 $(function() {
 
 	// Custom JS
-	$('.block .answer .a').on('click', function(){
+	// $('.block .answer .a').on('click', function(){
 		
-		// $('.block .answer .a').removeClass('active');
-		// $(this).parent().find('.a').removeClass('active');
-		// $(this).toggleClass('active');
-		$(this).css('background-color', 'tomato');
-	});
+	// 	// $('.block .answer .a').removeClass('active');
+	// 	// $(this).parent().find('.a').removeClass('active');
+	// 	// $(this).toggleClass('active');
+	// 	$(this).css('background-color', 'tomato');
+	// });
 
 
-	$('body').on('change', '.block .answer .a', function() { 
-		console.log('yes');
-	});
+	// $('body').on('change', '.block .answer .a', function() { 
+	// 	console.log('yes');
+	// });
 	
 
 
 });
-
-
-
-
-// new Vue({
-//   el: '#app',
-//   methods: {
-//     say: function (message) {
-//       //console.log(message)
-// 			axios({
-// 			    method: 'post',
-// 			    url: 'http://gen.test/pro.php',
-// 			    data: {
-// 			        firstName: 'Fred',
-// 			        lastName: 'Flintstone'
-// 			    }
-// 			})
-// 			.then(function(response) {
-
-// 			    console.log(response);
-
-// 			})
-// 			.catch(function (error) {
-// 			    console.log(error);
-// 			});
-//     }
-//   }
-// })
--- work/gulpfile.js
+++ work/gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/uikit/js/uikit.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
