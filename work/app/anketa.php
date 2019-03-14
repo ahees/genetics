9b8ea0bc44a738bd7a9261daed654ed855af7bb6
ahees <ahees@ex.ua>
2019-03-14 23:04:40 +0200

    1

---
+++ work/app/anketa.php
@@ -0,0 +1,3 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+echo $_POST['aq3'].'----'.$_POST['aq1'];
--- work/app/js/common.js
+++ work/app/js/common.js
@@ -1,9 +1,64 @@
+
 $(function() {
 
-	$('.sub').on('click', function(){
-
 		
+
+	$("#fanketa").on('change', '.block-a', function () {
+		var block_a = $(".block-a").serializeArray();
+
+		if (block_a.length >= 5) {
+			$(".next-block").addClass("active");
+		} else {
+			$(".next-block").removeClass("active");
+		}
+
+		$(".next-block.active").on('click', function () {
+			$(".block-a").fadeOut('slow', function () {
+				$(".block-b").fadeIn('slow');
+				$(".next-block").removeClass("active");
+				console.log('click');
+			});
+		});
+		//console.log(block_a.length);
+	});	
+	$("#fanketa").on('change', '.block-b', function () {
+		var block_b = $(".block-b").serializeArray();
+
+		if (block_b.length >= 6) {
+			$(".next-block").addClass("active");
+		} else {
+			$(".next-block").removeClass("active");
+		}
+
+		$(".next-block.active").on('click', function () {
+			$(".block-b").fadeOut('slow', function () {
+				$(".block-c").fadeIn('slow');
+				$(".next-block").removeClass("active").css("display", "none");
+				console.log('click');
+			});
+		});
+		//console.log(block_a.length);
+	});	
+
+	$("#fanketa").on("submit", function(e){
+		e.preventDefault();
+		var ddd = $('#fanketa').serializeArray();
+
+		$.ajax({
+			url: 'http://jobmax.server/anketa.php',
+			type: 'post',
+			data: ddd,
+			success: function (result) {
+				$('#result').html(result);
+			}
+		});
+		// console.log(data)
 	});
+	// var block_bb = $("#fanketa").serializeArray();
+	// console.log(block_bb)
+
+	
+	
 
 	// Custom JS
 	// $('.block .answer .a').on('click', function(){
