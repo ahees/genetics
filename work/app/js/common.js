31f379b75b9b335892f69ce1a7eb4e7aa691a7e1
Ahees <fdsparta@gmail.com>
2019-03-14 19:10:29 +0200

    1

--- work/app/js/common.js
+++ work/app/js/common.js
@@ -2,15 +2,7 @@ $(function() {
 
 	$('.sub').on('click', function(){
 
-		var data = $('#form1').serializeArray();
-
-		// var q1 = $("input[name=q1]");
-		// var checkedValue = q1.filter(":checked").val();
 		
-		
-		// console.log(checkedValue)
-		console.log(data)
-
 	});
 
 	// Custom JS
@@ -20,13 +12,10 @@ $(function() {
 		// $(this).parent().find('.a').removeClass('active');
 		// $(this).toggleClass('active');
 		
-	});
-
-
-	// $('body').on('change', '.block .answer .a', function() { 
-	// 	console.log('yes');
-	// });
 	
+
+
+
 
 
 });
